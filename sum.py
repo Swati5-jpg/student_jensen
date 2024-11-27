1b0c3e5723826960765e5f3a1ab53764124802d1
swasti sharma <swatis.sharma5@gmail.com>
2024-11-27 09:52:42 +0100

    new change

--- sum.py
+++ sum.py
@@ -2,3 +2,13 @@ nr1 = 5
 nr2 = 7
 sum = nr1 +nr2
 print(sum)
+
+def count_banana(fruits):
+    count = 0
+    for f in fruits:
+        if f == "banana":
+            count += 1
+    return count
+fruits =["banana","apple","appelsin","banana"]
+count =count_banana(fruits)
+print(count)  
